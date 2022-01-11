8cd9efd57223c82c2f3efe33e3a6a7cc951ac155
yessinkcharem <64277049+yessinkcharem@users.noreply.github.com>
2022-01-11 01:25:31 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,14 +3,23 @@ import numpy as np
 import pandas as pd
 import pickle
 import joblib
-# from sklearn.model_selection import RandomizedSearchCV
+import json
+from sklearn.model_selection import RandomizedSearchCV
 # from lightgbm import LGBMRegressor
 
 
 app = Flask(__name__)
 
 
+def process_resposne(input):
+    dict = {}
+    for key in input.keys() :
+        if input[key] == 'None':
+            dict[key] = np.NaN
+        else :
+            dict[key] = input[key]
 
+    return dict
 
 
 
@@ -19,7 +28,11 @@ app = Flask(__name__)
 def predict():
 
     #receiving the json
-    received = request.get_json()
+    input = request.get_json()
+
+
+
+
 
     """
     
@@ -30,11 +43,26 @@ def predict():
     
     """
     
-    print(type(received))
-    print((received))
+    print(type(input))
+    print((input))
+
+
+    #to assign to None values 
+    dict = {}
+    possible_values = ['None' , '', 'none' , 'null' , 'Null']
+    for key in input :
+        if input[key] in possible_values:
+            dict[key] = np.NaN
+        else :
+            dict[key] = input[key]
+
+
+    print(dict)
+
+
 
     #df will be passed through the pipeline
-    df = pd.DataFrame(received,index=[0])
+    df = pd.DataFrame(dict,index=[0])
 
 
 
@@ -52,14 +80,16 @@ def predict():
     y_pred = model.predict(to_predict)
 
     #the price
-    print(y_pred[0])
+    price = round(y_pred[0])
+    print(price)
 
+    output = {"price":price} 
 
-    return str((y_pred[0]))
+    return json.dumps(output)
     
 
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host = '0.0.0.0')
